d709816b5abe9f163ee285c5b6b8784c785bd443
Younjae Oh <younjae@kookmin.ac.kr>
2019-11-03 03:59:30 +0900

    using ||

---
+++ chap01/Digits.java
@@ -0,0 +1,19 @@
+package chap01;
+import java.util.Scanner;
+//2자리의 양수를 입력받음
+
+public class Digits {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int no;
+
+        System.out.println("2자리수의 정수를 입력하세요.");
+
+        do{
+            System.out.println("입력: ");
+            no = in.nextInt();
+        }while(no < 10 || no > 99);
+
+        System.out.println(no +"을(를) 입력했습니다.");
+    }
+}
