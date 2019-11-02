c6496b17da5e232d781b2486e79f8b5b500eeb54
Younjae Oh <younjae@kookmin.ac.kr>
2019-11-03 03:21:26 +0900

    using for loop

--- chap01/SumFor.java
+++ chap01/SumFor.java
@@ -1,4 +1,16 @@
 package chap01;
+import java.util.Scanner;
 
 public class SumFor {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum += i;
+        }
+
+        System.out.println("총합: " + sum);
+    }
 }
